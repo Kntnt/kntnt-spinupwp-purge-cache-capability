3c23c9d5bc7545c913c9fe80b7252788b8901173
Thomas Barregren <thomas@kntnt.com>
2023-08-16 13:26:40 +0200

    Create kntnt-spinupwp-purge-cache-capability.php

---
+++ kntnt-spinupwp-purge-cache-capability.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt SpinupWP Purge Cache Capability
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Changes the capability for empty cache through SpinupWP helper plugin to kntnt-spinupwp-purge-cache.
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+add_filter( 'spinupwp_purge_cache_capability', function() {
+  return 'kntnt_spinupwp_purge_cache';
+} );
